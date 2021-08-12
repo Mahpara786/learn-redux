1e5e4320a7b1bab5d3dbcb4b978e631ba73c0d5a
Mahpara786 <mahpara8604@gmail.com>
2021-08-12 19:37:47 +0530

    counter

--- src/index.js
+++ src/index.js
@@ -2,47 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore} from "redux";
+import { createStore , combineReducers } from "redux";
+import allReducers from "./Reducers/index";
+import { Provider } from "react-redux";
 
 
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-const increment = () => {
-  return {
-    type : "INCREMENT"
-  }
-}
-const decrement = () => {
-  return {
-    type : "DECREMENT"
-  }
-}
 
-const counter = (state=0, action) => {
-  switch(action.type){
-    case "INCREMENT" :
-      return state +1
-    case "DECREMENT" :
-      return state-1 
-  }
-}
-let store = createStore(counter)
-
-store.subscribe(() => console.log(store.getState()));
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-store.dispatch(increment());
-
-  
-  
 ReactDOM.render(
+  <Provider store={store}>             
   <React.StrictMode>
     <App />
-
   </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-
- 
